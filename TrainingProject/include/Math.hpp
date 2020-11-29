483ec2f4e228b14b04f5c1690d5dc20ac9f6a26d
KromkaAT <74835401+KromkaAT@users.noreply.github.com>
2020-11-29 21:24:53 +0100

    Kalkulator :).

--- TrainingProject/include/Math.hpp
+++ TrainingProject/include/Math.hpp
@@ -1,1 +1,110 @@
+#ifndef TP_MATH_HPP
+#define TP_MATH_HPP
+#include <cmath>
 
+
+namespace dzialania
+{
+	inline int dodawanie(int a, int b)
+	{
+		int x{a + b};
+		return x;
+	}
+	inline int odejmowanie(int a, int b)
+	{
+
+		int x{a - b};
+		return x;
+	}
+	inline int mnozenie(int a, int b)
+	{
+		int x{a * b};
+		return x;
+	}
+	inline int dzielenie(int a, int b)
+	{
+		int x{a / b};
+		return x;
+	}
+	inline int dzielenie_z_reszta(int a, int b)
+	{
+		int x{a % b};
+		return x;
+	}
+	inline double potega(int a, int b)
+	{
+		double x{ std::pow(a, b) };
+		return x;
+	}
+	inline double pierwiastek(int a)
+	{
+		double x{ std::sqrt(a) };
+		return x;		
+	}
+	inline double pitagoras(int a, int b)
+	{
+		double c{ std::pow(a, 2) + std::pow(b, 2) };
+		double x{ std::sqrt(c) };
+		return x;
+	}
+	inline double obwod_kwadratu(double a)
+	{
+		double x{ a * 4 };
+		return x;
+	}
+	inline int obwod_prostokota(int a, int b)
+	{
+		int x{ (a * 2) + (b * 2) };
+		return x;
+	}
+	inline int obwod_trojkata_rownobocznego(int a)
+	{
+		int x{ a * 3 };
+		return x;
+	}
+	inline int obwod_trojkata_roznobocznego(int a, int b, int c)
+	{
+		int x{ a + b + c };
+		return x;
+	}
+	inline int obwod_trojkata_rownoramiennego(int a, int b)
+	{
+		int x{ a + (b * 2) };
+		return x;
+	}
+	inline int pole_kwadratu(int a)
+	{
+		int x{ a * a };
+		return x;
+	}
+	inline int pole_prostokata(int a, int b)
+	{
+		int x{ a * b };
+		return x;
+	}
+	inline double nwd(double a, double b)
+	{
+		double x{};
+		if (a != b)
+		{
+			if (a > b)
+			{
+				a = a - b;
+			}
+			else
+			{
+				b = b - a;
+			}
+
+		}
+		else
+			return a;
+	}
+	inline double nww(double a, double b)
+	{
+		double x{ a * b / nwd(a, b) };
+		return x;
+	}
+}
+
+#endif
--- TrainingProject/src/Main.cpp
+++ TrainingProject/src/Main.cpp
@@ -1,8 +1,109 @@
 #include "../include/Math.hpp"
 #include <iostream>
+#include <iterator>
 
-int main()
-{
-	std::cout << "nie dla kofickiego";
-	return 0;
-}
+	int main()
+	{
+		int dzialanie{};
+		int a{}, b{}, c{};
+		std::cout << "Witaj w kalkulatorze. \n Wybierz dzialanie :\n 1. Dodawanie \n 2. Odejmowanie \n 3. Mnozenie \n 4. Dzielenie \n 5. Dzielenie z reszta \n";
+		std::cout << " 6. Potega\n 7. Pierwiastek\n 8. Twierdzenie Pitagorasa\n 9. Obwod kwadratu\n 10. Obwod prostokata\n 11. Obwod trojkata rownobocznego\n 12. Obwod trojkata roznobocznego\n 13. Obwod trojkata rownoramiennego\n 14. Pole kwadratu\n 15. Pole prostokata\n 16. NWD\n 17. NWW\n";
+		do
+		{
+			std::cin >> dzialanie;
+			switch (dzialanie)
+			{
+			case 1:
+				std::cout << "\nPodaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::dodawanie(a, b) << "\n\n";
+				break;
+			case 2:
+				std::cout << "\nPodaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::odejmowanie(a, b) << "\n\n";
+				break;
+			case 3:
+				std::cout << "\nPodaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::mnozenie(a, b) << "\n\n";
+				break;
+			case 4:
+				std::cout << "\nPodaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::dzielenie(a, b) << "\n\n";
+				break;
+			case 5:
+				std::cout << "\nPodaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::dzielenie_z_reszta(a, b) << "\n\n";
+				break;
+			case 6:
+				std::cout << "\nPodaj podstawe potegi i wykladnik potegi : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::potega(a, b) << "\n\n";
+				break;
+			case 7:
+				std::cout << "\nPodaj liczbe a (pierwiastek 2 stopnia) :\n";
+				std::cin >> a;
+				std::cout << "Wynik : " << dzialania::pierwiastek(a) << "\n\n";
+				break;
+			case 8:
+				std::cout << "\nPodaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::pitagoras(a, b) << "\n\n";
+				break;
+			case 9:
+				std::cout << "\n Podaj liczbe a : \n";
+				std::cin >> a;
+				std::cout << "Wynik : " << dzialania::obwod_kwadratu(a) << "\n\n";
+				break;
+			case 10:
+				std::cout << "\n Podaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::obwod_prostokota(a, b) << "\n\n";	
+				break;
+			case 11:
+				std::cout << "\n Podaj liczbe a  : \n";
+				std::cin >> a;
+				std::cout << "Wynik : " << dzialania::obwod_trojkata_rownobocznego(a) << "\n\n";
+				break;
+			case 12:
+				std::cout << "\n Podaj liczbe a, b i c : \n";
+				std::cin >> a >> b >> c;
+				std::cout << "Wynik : " << dzialania::obwod_trojkata_roznobocznego(a, b, c) << "\n\n";
+				break;
+			case 13:
+				std::cout << "\n Podaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::obwod_trojkata_rownoramiennego(a, b) << "\n\n";
+				break;
+			case 14:
+				std::cout << "\n Podaj liczbe a : \n";
+				std::cin >> a;
+				std::cout << "Wynik : " << dzialania::pole_kwadratu(a) << "\n\n";
+				break;
+			case 15:
+				std::cout << "\n Podaj liczbe a i b : \n";
+				std::cin >> a;
+				std::cout << "Wynik : " << dzialania::pole_prostokata(a, b) << "\n\n";
+				break;
+			case 16:
+				std::cout << "\n Podaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::nwd(a, b) << "\n\n";
+				break;
+			case 17:
+				std::cout << "\n Podaj liczbe a i b : \n";
+				std::cin >> a >> b;
+				std::cout << "Wynik : " << dzialania::nww(a, b) << "\n\n";
+				break;
+			default:
+				std::cout << "\nNie wolno!\n\n";
+			}
+		} while (true);
+
+		return 0;
+	}
+
+	
